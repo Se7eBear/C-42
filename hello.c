88c2f8910f1faa21cdbe0c09e05aac74cea4ecd8
Se7eBear <brunose7e@gmail.com>
2026-02-19 23:32:07 -0300

    feat: add sum script in C

---
+++ hello.c
@@ -0,0 +1,14 @@
+int main(void){
+    int a = 10;
+    int b = 11;
+    int soma;
+
+    if(a < b){
+        soma = a + b;
+        printf("o Resultado de a + b deu %d", soma);
+
+    }else{
+        printf("Alelo");
+    }
+    return 0;
+}
