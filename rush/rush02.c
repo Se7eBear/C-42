2d84d9b07d9210d68aca6ab3cc33188133f99f2a
Bruno Sete <brunose7e@gmail.com>
2026-03-01 00:43:34 -0300

    Organizando as pastas e adicionado o rush

--- rush/rush02.c
+++ rush/rush02.c
@@ -18,12 +18,8 @@ void rush(int x, int y)
 		{
 			if (coluna == 1 && linha == 1)
 				ft_putchar('A');
-			else if (coluna == y)
+			else if (coluna == x)
 				ft_putchar('A');
-			else if (linha == y && coluna == x )
-				ft_putchar('C');
-			else if (linha == x)
-				ft_putchar('C');
 			else
 				ft_putchar('B');				
 			coluna++;
@@ -33,9 +29,53 @@ void rush(int x, int y)
 	}
 }
 
-void mostrar_C(int x, int y)
+void mostrar_B(int x, int y)
+{
+	int linha;
+	int coluna;
+	linha = 1;
+	while 	(linha <= y)
+	{
+		coluna = 1;
+		while (coluna <= x){
+			if (coluna == 1 && linha == y)
+				ft_putchar('B');
+			else if (coluna == x)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+				coluna++;
+		}
+			ft_putchar('\n');
+			linha++;
+	}
+}
+void	mostrar_c(int x, int y)
+{
+	int coluna;
+	int linha;
+	linha = 1;
+	while (linha <= y)
+	{	coluna = 1;
+		while(coluna <= x)
+		{
+			if (linha == y && coluna == 1)
+				ft_putchar('C');
+			else if (coluna == x)
+				ft_putchar('C');
+			else
+				ft_putchar('B');
+				coluna++;
+		}
+		ft_putchar('\n');
+		linha++;
+	}
+}
 int main(void)
 {
 	rush (5,1);
+	mostrar_B (5, 1);
+	mostrar_c(5, 1);
 	return (0);
+}
 }
