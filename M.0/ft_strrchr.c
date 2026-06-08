840e0c81c817e96aaf4aad6e60a55420c3a256da
Bruno Sete <sete@MacBook-Air-de-Bruno.local>
2026-06-08 12:20:07 -0300

    fix: delete some lines

--- M.0/ft_strrchr.c
+++ M.0/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: brde-car <brde-car@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 00:33:50 by brde-car          #+#    #+#             */
-/*   Updated: 2026/06/02 03:34:55 by brde-car         ###   ########.fr       */
+/*   Updated: 2026/06/08 12:19:32 by brde-car         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ char	*ft_strrchr(const char *s, int c)
 	aux = (char)c;
 	i = 0;
 	while (s[i] != '\0')
-	{
 		i++;
-	}
 	while (i >= 0)
 	{
 		if (s[i] == aux)
