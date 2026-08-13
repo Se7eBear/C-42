44a75c287ec934eeb70e59af9d8650b7533f3886
Bruno Sete <brunose7e@gmail.com>
2026-08-12 23:35:57 -0300

    feat: add ex4 in python

---
+++ M.2/module0/ex4/ft_plant_age.py
@@ -0,0 +1,7 @@
+def ft_plant_age() -> None:
+    ask1 = int(input("Enter plant age in days: "))
+    if ask1 > 60:
+        print("Plant is ready to harvest!")
+    else:
+        print("Plants needs more time to grow")
+    
