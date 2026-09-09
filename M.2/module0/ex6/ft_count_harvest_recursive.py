2806f58f1c184aafc95fdd757ce61571fbf9f589
Bruno De Carvalho Sete <brde-car@c1r3p2.42sp.org.br>
2026-09-09 02:56:47 -0300

    fix some things

--- M.2/module0/ex6/ft_count_harvest_recursive.py
+++ M.2/module0/ex6/ft_count_harvest_recursive.py
@@ -1,7 +1,11 @@
-def ft_count_harvest_recursive(j: int, i: int) -> None:
-    j = 0
-    if j > i:
-        print("Harvest time!")
-        return
-    print(f"Day {j}")
-    ft_count_harvest_recursive(j + 1, i)
+def ft_count_harvest_recursive():
+    days = int(input("Enter the number of days: "))
+
+    def ft_count(j: int, i: int) -> None:
+        if j > i:
+            print("Harvest time!")
+            return
+        print(f"Day {j}")
+        ft_count(j + 1, i)
+
+    ft_count(1, days)
